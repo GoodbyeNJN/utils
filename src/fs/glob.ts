cc8a22cd9d60dc4f1e740e809f5d1bce3f3cea39
GoodbyeNJN <cc@fuckwall.cc>
2025-11-11 20:12:28 +0800

    feat: add support for patterns in options object

--- src/fs/glob.ts
+++ src/fs/glob.ts
@@ -1,25 +1,56 @@
 import { glob as tinyGlob, globSync as tinyGlobSync } from "tinyglobby";
 
-import type { GlobOptions } from "tinyglobby";
+import { isArray, isString, omit } from "@/remeda";
 
-export const glob = (
-    patterns: string | readonly string[],
-    options?: Omit<GlobOptions, "patterns">,
-): Promise<string[]> =>
-    tinyGlob(patterns, {
-        expandDirectories: false,
-        ...options,
-    });
+import type { GlobOptions as TinyGlobOptions } from "tinyglobby";
 
-export const globSync = (
-    patterns: string | readonly string[],
-    options: Omit<GlobOptions, "patterns"> = {},
-): string[] =>
-    tinyGlobSync(patterns, {
-        expandDirectories: false,
-        ...options,
-    });
+export type { FileSystemAdapter } from "tinyglobby";
+
+export interface GlobOptions extends Omit<TinyGlobOptions, "patterns"> {
+    /**
+     * Provide patterns as the first argument instead.
+     */
+    patterns: string | string[];
+}
 
 export { convertPathToPattern, escapePath, isDynamicPattern } from "tinyglobby";
 
-export type { FileSystemAdapter, GlobOptions } from "tinyglobby";
+const normalizeArgs = (
+    patternsOrOptions: string | string[] | GlobOptions,
+    maybeOptions?: Omit<GlobOptions, "patterns">,
+) => {
+    const patternsAsArgument = isString(patternsOrOptions) || isArray(patternsOrOptions);
+
+    const patterns = patternsAsArgument ? patternsOrOptions : patternsOrOptions.patterns;
+    const options = patternsAsArgument ? maybeOptions || {} : omit(patternsOrOptions, ["patterns"]);
+
+    return { patterns, options };
+};
+
+export function glob(
+    patterns: string | string[],
+    options?: Omit<GlobOptions, "patterns">,
+): Promise<string[]>;
+export function glob(options: GlobOptions): Promise<string[]>;
+export function glob(...args: [any, any?]): Promise<string[]> {
+    const { patterns, options } = normalizeArgs(...args);
+
+    return tinyGlob(patterns, {
+        expandDirectories: false,
+        ...options,
+    });
+}
+
+export function globSync(
+    patterns: string | string[],
+    options?: Omit<GlobOptions, "patterns">,
+): string[];
+export function globSync(options: GlobOptions): string[];
+export function globSync(...args: [any, any?]): string[] {
+    const { patterns, options } = normalizeArgs(...args);
+
+    return tinyGlobSync(patterns, {
+        expandDirectories: false,
+        ...options,
+    });
+}
